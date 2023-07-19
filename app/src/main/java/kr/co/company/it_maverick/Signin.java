f195c293e73c75602030c3925557976232b415a1
yeseong Lee <yslee4232@naver.com>
2023-07-19 16:19:19 +0900

    message

---
+++ app/src/main/java/kr/co/company/it_maverick/Signin.java
@@ -0,0 +1,13 @@
+package kr.co.company.it_maverick;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
---
+++ app/src/main/java/kr/co/company/it_maverick/main_home.java
@@ -0,0 +1,14 @@
+package kr.co.company.it_maverick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home);
+    }
+}
